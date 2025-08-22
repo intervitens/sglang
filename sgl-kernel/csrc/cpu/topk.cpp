568d019473bb26056cf055b338b9d7cdbcc1bf65
intervitens <intervitens@tutanota.com>
2025-08-22 12:35:25 +0000

    Support glm4.5 MoE on CPU

--- sgl-kernel/csrc/cpu/topk.cpp
+++ sgl-kernel/csrc/cpu/topk.cpp
@@ -651,6 +651,12 @@ std::tuple<at::Tensor, at::Tensor> biased_grouped_topk_cpu(
   CPU_DISPATCH_REDUCED_FLOATING_TYPES_EXT(st, correction_bias.scalar_type(), "biased_grouped_topk_kernel", [&] {
     TORCH_CHECK(topk == 8, "Unexpected topk: ", topk);
     switch (num_experts) {
+      case 128:
+        LAUNCH_BIASED_GROUPED_TOPK_KERNEL(128, 8);
+        break;
+      case 160:
+        LAUNCH_BIASED_GROUPED_TOPK_KERNEL(160, 8);
+        break;
       case 256:
         LAUNCH_BIASED_GROUPED_TOPK_KERNEL(256, 8);
         break;
