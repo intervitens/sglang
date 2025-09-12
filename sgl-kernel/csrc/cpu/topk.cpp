ee46afaf68cee4574f39f272c317e5bebf3f8a52
intervitens <intervitens@tutanota.com>
2025-09-12 21:46:51 +0000

    moonlight 16b support

--- sgl-kernel/csrc/cpu/topk.cpp
+++ sgl-kernel/csrc/cpu/topk.cpp
@@ -649,19 +649,42 @@ std::tuple<at::Tensor, at::Tensor> biased_grouped_topk_cpu(
   at::Tensor topk_ids = at::empty({num_tokens, topk}, hidden_states.options().dtype(at::kInt));
 
   CPU_DISPATCH_REDUCED_FLOATING_TYPES_EXT(st, correction_bias.scalar_type(), "biased_grouped_topk_kernel", [&] {
-    TORCH_CHECK(topk == 8, "Unexpected topk: ", topk);
-    switch (num_experts) {
-      case 128:
-        LAUNCH_BIASED_GROUPED_TOPK_KERNEL(128, 8);
+    switch (topk) {
+      case 8:
+        switch (num_experts) {
+          case 128:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(128, 8);
+            break;
+          case 160:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(160, 8);
+            break;
+          case 256:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(256, 8);
+            break;
+          default:
+            TORCH_CHECK(false, "Unexpected num_experts: ", num_experts);
+        }
         break;
-      case 160:
-        LAUNCH_BIASED_GROUPED_TOPK_KERNEL(160, 8);
-        break;
-      case 256:
-        LAUNCH_BIASED_GROUPED_TOPK_KERNEL(256, 8);
+      case 6:
+        switch (num_experts) {
+          case 64:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(64, 6);
+            break;
+          case 128:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(128, 6);
+            break;
+          case 160:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(160, 6);
+            break;
+          case 256:
+            LAUNCH_BIASED_GROUPED_TOPK_KERNEL(256, 6);
+            break;
+          default:
+            TORCH_CHECK(false, "Unexpected num_experts: ", num_experts);
+        }
         break;
       default:
-        TORCH_CHECK(false, "Unexpected num_experts: ", num_experts);
+        TORCH_CHECK(false, "Unexpected topk: ", topk);
     }
   });
   return std::make_tuple(topk_weights, topk_ids);
